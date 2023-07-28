8e1913740c6a7eb447a3c8350fef8965eddce7fc
Joung,Jae-Hwan <juj6584@naver.com>
2023-07-28 16:54:50 +0900

    0

---
+++ Assets/Scripts/LIstbehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class LIstbehavior : SerializedMonoBehaviour
+{
+    public Dictionary<float, float> aaa = new Dictionary<float, float>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
