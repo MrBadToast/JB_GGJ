b2b5c25a8b65b96b3e85650fd7150848b7b89d72
BadToast <ship2042@gmail.com>
2023-07-28 20:46:49 +0900

    UIElements

--- Assets/Scripts/UI_ItemPanel.cs
+++ Assets/Scripts/UI_ItemPanel.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_ItemPanel : MonoBehaviour
 {
     [SerializeField] private GameObject[] itemSlots;
 
-    public void SetSlots()
+    public void SetSlots(Item_Data[] dataSet)
     {
-
+       for(int i = 0; i < dataSet.Length; i++)
+        {
+            itemSlots[i].GetComponent<Image>().sprite = dataSet[i].Item_sprite;
+        }
     }
 }
