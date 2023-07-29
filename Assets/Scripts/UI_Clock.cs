511e652ea30f818dee31014abfeda21f6e444812
BadToast <ship2042@gmail.com>
2023-07-29 21:58:24 +0900

    Enterrance Sprite

--- Assets/Scripts/UI_Clock.cs
+++ Assets/Scripts/UI_Clock.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 
 public class UI_Clock : MonoBehaviour
 {
+    [SerializeField] Transform hour;
+    [SerializeField] Transform minute;
     [SerializeField] new Image renderer;
     [SerializeField] DOTweenAnimation doAnimation;
 
     public void SetClock(float value)
     {
         renderer.material.SetFloat("_Value", value);
+        hour.rotation = Quaternion.Euler(0f, 0f, value * 360f);
+        minute.rotation = Quaternion.Euler(0f, 0f, value * 360f * 12f);
     }
 
     public void OpenClock()
